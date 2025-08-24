472097b2a3a7b11dbd4170c552194d154c761c97
DavoDev-Hub <juanpajmz13@gmail.com>
2025-08-24 12:24:08 -0600

    Interfaces

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,5 +1,7 @@
 import "./style.css";
 //import "./topics/01-basic-types";
+import "./topics/02-object-interface";
+
 document.querySelector<HTMLDivElement>("#app");
 
 const app = document.querySelector<HTMLDivElement>("#app");
